479018db337fcf92c487fe173dc24b5edfe55313
Ozi <oguzkaanyildiz@Ozis-MacBook-Air.local>
2022-05-28 12:50:21 +0200

    Checking the API

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+// api key: f3ea3c53
+
+// Fetch the Movie Data from the API
+async function fetchData(searchTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'f3ea3c53',
+            s: searchTerm
+        }
+    });
+    console.log(response.data);
+};
+
+function onInput(event) {
+    setTimeout(() => {
+        fetchData(event.target.value)
+    }, 1000)
+}
+
+const input1 = document.getElementById("input1");
+input1.addEventListener('input', onInput);
