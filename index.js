83bc6c37b4c2dcec43459b47a8c42437c8475c95
Ozi <oguzkaanyildiz@Ozis-MacBook-Air.local>
2022-05-29 19:56:53 +0200

    Handling the movie selection

--- index.js
+++ index.js
@@ -14,38 +14,67 @@ async function fetchData(searchTerm) {
     return response.data.Search;
 };
 
-// Create an search-input element with dropdown feature
+// Create a search-input element with dropdown feature
 const root = document.querySelector('.autocomplete');
 root.innerHTML = `
-<label><b>Search for a Movie</b><label/>
-<input class='input' />
-<div class='dropdown'>
-<div class='dropdown-menu'>
-<div class='dropdown-content results'></div>
-</div>
-</div>
+    <label><b>Search for a Movie</b><label/>
+    <input class='input' />
+    <div class='dropdown'>
+        <div class='dropdown-menu'>
+            <div class='dropdown-content results'></div>
+        </div>
+    </div>
 `;
 const dropdown = document.querySelector('.dropdown');
 const resultsWrapper = document.querySelector('.results');
 
 // Delay the search input
 // List the movies in the dropdown menu after searching
-const input = document.getElementById('input');
+const input = document.querySelector('input');
 async function onInput(event) {
     const movies = await fetchData(event.target.value)
+
+    if (!movies.length) {
+        dropdown.classList.remove('is-active');
+        return;
+    }
 
     dropdown.classList.add('is-active');
 
     for (let movie of movies) {
         const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
         option.classList.add('dropdown-item')
         option.innerHTML =
-            `<img src="${movie.Poster}" />
+            `<img src="${imgSrc}" />
             ${movie.Title}
             `;
+
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');
+            input.value = movie.Title
+            onMovieSelect(movie);
+        })
+
 
         resultsWrapper.appendChild(option);
     }
 };
 
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active')
+    }
+});
+
+async function onMovieSelect(movie) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'f3ea3c53',
+            i: movie.imdbID
+        }
+    })
+    console.log(response.data);
+}
